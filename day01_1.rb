ceb6746e6a5d3f9359e046e22c1639e6aa7080d0
Alex Musayev <alex.musayev@gmail.com>
2024-12-01 12:17:17 +0100

    1, 2

---
+++ day01_1.rb
@@ -0,0 +1,11 @@
+list1, list2 = File.read("./data.txt")
+  .split("\n")
+  .map { _1.split(/\s+/) }
+  .map { [Integer(_1), Integer(_2)] }
+  .transpose
+
+sorted1 = list1.sort
+sorted2 = list2.sort
+
+distance = sorted1.zip(sorted2).map { (_1 - _2).abs }.sum
+puts distance
---
+++ day01_2.rb
@@ -0,0 +1,8 @@
+list1, list2 = File.read("./data.txt")
+  .split("\n")
+  .map { _1.split(/\s+/) }
+  .map { [Integer(_1), Integer(_2)] }
+  .transpose
+
+list2_tally = list2.tally
+list1.map { _1 * list2_tally[_1].to_i }.sum
