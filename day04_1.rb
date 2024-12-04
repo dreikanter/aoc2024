670ee8b0769cacb886b08f60a9ce8b77fd6a38db
Alex Musayev <alex.musayev@gmail.com>
2024-12-04 21:25:51 +0100

    Use alternative approach

--- day04_1.rb
+++ day04_1.rb
@@ -1,35 +1,37 @@
 WORD = "XMAS"
+WORD_VARIATIONS = /(?=(#{WORD}|#{WORD.reverse}))/
 
-# rows = File.read("./day04_data.txt").split("\n").map(&:chars)
-rows = File.read("./day04_data_test.txt").split("\n").map(&:chars)
+rows = File.read("./day04_data.txt").split("\n").map(&:chars)
+# rows = File.read("./day04_data_test.txt").split("\n").map(&:chars)
 
-row_length = rows.first.length
+width = rows.first.length
+height = rows.length
 
-words = (0..rows.length.pred).map do |y|
-  (0..row_length.pred).map do |x|
-    horizontal = (x..x + WORD.length.pred).map { rows[y][_1] }.join
-    vertical = (y..y + WORD.length.pred).map { rows.dig(_1, x) }.join
-    diagonal1 = (0..WORD.length.pred).map { rows.dig(y + _1, x + _1) }.join
-    diagonal2 = (0..WORD.length.pred).map { rows.dig(y + _1, x - _1) }.join
+horizontal = rows.map { _1.join }
+vertical = (0..width.pred).map { |x| (0..height.pred).map { |y| rows[y][x] }.join }
 
-    [
-      [horizontal, 'horizontal', x, y],
-      [vertical, 'vertical', x, y],
-      [diagonal1, 'diagonal1', x, y],
-      [diagonal2, 'diagonal2', x, y]
-    ]
+forward_diagonals = {}
 
-    # [horizontal, vertical, diagonal1, diagonal2]
+(0..width.pred).each do |x|
+  (0..height.pred).each do |y|
+    (forward_diagonals[x - y] ||= []) << rows[y][x]
   end
 end
 
-# puts words.flatten.count { _1 == WORD || _1 == WORD.reverse }
+backward_diagonals = {}
 
-pp rows
-
-count = 0
-words.flatten(2).each do |word, direction, x, y|
-  if word == WORD || word == WORD.reverse
-    puts "#{count += 1} Found #{word} at (#{x},#{y}) going #{direction}"
+(0..width.pred).each do |x|
+  (0..height.pred).each do |y|
+    (backward_diagonals[x + y] ||= []) << rows[y][x]
   end
 end
+
+diagonals = forward_diagonals.values.concat(backward_diagonals.values).map(&:join)
+
+sequences = [
+  horizontal,
+  vertical,
+  diagonals
+].flatten
+
+puts sequences.sum { _1.scan(WORD_VARIATIONS).count }
