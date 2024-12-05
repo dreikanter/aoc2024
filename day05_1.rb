2306147f2e133c95a0c6b0291b4224309008c9ff
Alex Musayev <alex.musayev@gmail.com>
2024-12-05 11:04:57 +0100

    Clean up

--- day05_1.rb
+++ day05_1.rb
@@ -2,13 +2,11 @@ rules = File.read("day05_rules.txt").split("\n").map { _1.split("|").map(&:to_i)
 sequences = File.read("day05_sequences.txt").split("\n").map { _1.split(",").map(&:to_i) }
 
 correct = sequences.filter do |sequence|
-  pp sequence
   result = (0..sequence.length.pred).all? do |first|
     (first.succ..sequence.length.pred).all? do |last|
-      # puts "#{first},#{last} | #{rules.include?([sequence[first], sequence[last]])}"
       rules.include?([sequence[first], sequence[last]])
     end
-  end.tap { puts _1 }
+  end
 end
 
 puts correct.sum { _1[(_1.length / 2)] }
