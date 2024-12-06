ee4647f001866313b437ef463eabebcfc0289869
Alex Musayev <alex.musayev@gmail.com>
2024-12-06 14:46:35 +0100

    Clean up

--- day06_1.rb
+++ day06_1.rb
@@ -5,13 +5,11 @@ class Lab
     @data = data
   end
 
-  def obstacle?(position)
-    x, y = position
+  def obstacle?(x, y)
     data.dig(y, x) == "#"
   end
 
-  def outside?(position)
-    x, y = position
+  def outside?(x, y)
     x < 0 || x >= width || y < 0 || y >= height
   end
 
@@ -25,22 +23,22 @@ class Lab
 end
 
 class Guard
-  attr_reader :x, :y, :direction, :distinct_positions
+  attr_reader :x, :y, :dx, :dy, :distinct_positions
 
   UP = [0, -1]
   DOWN = [0, 1]
   LEFT = [-1, 0]
   RIGHT = [1, 0]
 
-  def initialize(x, y, direction = UP)
+  def initialize(x, y)
     @x = x
     @y = y
-    @direction = direction
+    @dx, @dy = UP
     @distinct_positions = Set.new([[x, y]])
   end
 
   def turn_right
-    @direction = case direction
+    @dx, @dy = case [dx, dy]
     when LEFT
       UP
     when UP
@@ -59,17 +57,17 @@ class Guard
   end
 
   def next_position
-    [x + direction[0], y + direction[1]]
+    [x + dx, y + dy]
   end
 
   def move
-    @x += direction[0]
-    @y += direction[1]
+    @x += dx
+    @y += dy
     @distinct_positions << position
   end
 end
 
-data = File.read("./day06_1_data.txt").split("\n").map(&:chars)
+data = File.read("./day06_data.txt").split("\n").map(&:chars)
 
 initial_y = data.find_index { _1.include? "^" }
 initial_x = data[initial_y].find_index { _1 == "^" }
@@ -78,9 +76,9 @@ guard = Guard.new(initial_x, initial_y)
 lab = Lab.new(data)
 
 loop do
-  break if lab.outside?(guard.next_position)
+  break if lab.outside?(*guard.next_position)
 
-  if lab.obstacle?(guard.next_position)
+  if lab.obstacle?(*guard.next_position)
     guard.turn_right
   else
     guard.move
