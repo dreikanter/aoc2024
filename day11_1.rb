8edd0bcb29602a4fade63594e3eb4e4f68983c01
Alex Musayev <alex.musayev@gmail.com>
2024-12-11 13:50:33 +0100

    Clean up

--- day11_1.rb
+++ day11_1.rb
@@ -1,4 +1,3 @@
-# numbers = File.read("day11_test_data.txt").split(" ").map { Integer(_1) }
 numbers = File.read("day11_data.txt").split(" ").map { Integer(_1) }
 
 25.times do
@@ -7,22 +6,20 @@ numbers = File.read("day11_data.txt").split(" ").map { Integer(_1) }
   numbers.each do |number|
     if number == 0
       result << 1
-      next
+    else
+      digits = Math.log10(number).to_i + 1
+
+      if digits.even?
+        divisor = 10**(digits / 2)
+        result << number / divisor
+        result << number % divisor
+      else
+        result << number * 2024
+      end
     end
 
-    digits = Math.log10(number).to_i + 1
-
-    if digits.even?
-      divisor = 10**(digits / 2)
-      result << number / divisor
-      result << number % divisor
-      next
-    end
-
-    result << number * 2024
+    numbers = result
   end
-
-  numbers = result
 end
 
 puts numbers.count
